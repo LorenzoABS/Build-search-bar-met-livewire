15d4f42ef8b26d9222247d5912ee4fad9bad0c30
LorenzoABS <150017251+LorenzoABS@users.noreply.github.com>
2025-06-10 13:15:43 +0200

    3:11

---
+++ resources/views/livewire/search-bar.blade.php
@@ -0,0 +1,19 @@
+<div id="search-bar">
+    <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+   </form>
+   <div class="dropdown-menu d-block py-0">
+    <div class="px-3 py-1 border-bottom">
+        <div class="d-flex flex-column ml-3">
+            <span>Name</span>
+            <small>@email</small>
+        </div>
+    </div>
+    <div class="px-3 py-1">
+        <div class="d-flex flex-column ml-3">
+            <span>Name</span>
+            <small>@email</small>
+        </div>
+    </div>
+   </div>
+</div>
